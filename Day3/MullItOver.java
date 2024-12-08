2756678007949e7eb616ccfc06e0611c356f7728
Max Cullen Feng <z5255095@student.unsw.edu.au>
2024-12-08 14:23:58 +1100

    day3 done

--- Day3/MullItOver.java
+++ Day3/MullItOver.java
@@ -11,16 +11,25 @@ import java.util.regex.Matcher;
 public class MullItOver {
     public static void main(String[] args) {
         int mulTotal = 0;
-        String regex = "mul\\(\\d{1,3},\\d{1,3}\\)"; 
+        String regex = "mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)"; // regex for mul and do/don't
         Pattern pattern = Pattern.compile(regex); 
-  
+        boolean mulEnabled = true;
         try (Scanner scanner = new Scanner(Paths.get("Day3/corrupted.txt"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 Matcher matcher = pattern.matcher(line);
-                while (matcher.find()) { 
-                    String[] splits = matcher.group().split("[mul(),\\s)]"); // easy separation, integers go into index 4 and 5
-                    mulTotal += Integer.parseInt(splits[4]) * Integer.parseInt(splits[5]);
+                
+                while (matcher.find()) {
+                    if (matcher.group().contains("do()")) {
+                        mulEnabled = true;
+                    } else if (matcher.group().contains("don't()")) {
+                        mulEnabled = false;
+                    } else {
+                        if (mulEnabled) {
+                            String[] splits = matcher.group().split("[mul(),\\s)]"); // easy separation, integers go into index 4 and 5
+                            mulTotal += Integer.parseInt(splits[4]) * Integer.parseInt(splits[5]);
+                        }
+                    }
                 } 
             }
         } catch (Exception e) {
