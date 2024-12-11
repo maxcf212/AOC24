0778ecfd5550d9afebc827e36fefe8cb94d036fa
Max Cullen Feng <z5255095@student.unsw.edu.au>
2024-12-11 22:28:42 +1100

    day4 done

--- Day4/CeresSearch.java
+++ Day4/CeresSearch.java
@@ -32,10 +32,13 @@ public class CeresSearch
         int rows = grid.size();
 
         int count = 0; // count of xmas
+        int count2 = 0; // count of x-mas
 
-        // this is gross but works
+        
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
+                // this is gross but works
+                // find each valid starting x and check surrounding paths for 'm-a-s' followers
                 if (grid.get(i).get(j) == 'X') {
                     //s
                     if ((i <= rows - 4) && grid.get(i+1).get(j) == 'M' && grid.get(i+2).get(j) == 'A' && grid.get(i+3).get(j) == 'S') {
@@ -69,9 +72,26 @@ public class CeresSearch
                     if ((j - 3 >= 0) && (i <= rows - 4) && grid.get(i+1).get(j-1) == 'M' && grid.get(i+2).get(j-2) == 'A' && grid.get(i+3).get(j-3) == 'S') {
                         count += 1;
                     }
-                } 
+                }
+                
+                //extra gross but works
+                //find each a and check surrounding diagonols for M + S pairs
+                if (grid.get(i).get(j) == 'A') {
+                    if (i > 0 && i < rows - 1 && j > 0 && j < cols - 1) {
+                        if (grid.get(i-1).get(j-1) == 'M' && grid.get(i+1).get(j+1) == 'S') {
+                             if ((grid.get(i-1).get(j+1) == 'M' && grid.get(i+1).get(j-1) == 'S') || (grid.get(i-1).get(j+1) == 'S' && grid.get(i+1).get(j-1) == 'M')) {
+                                count2 += 1;
+                             }
+                        } else if (grid.get(i-1).get(j-1) == 'S' && grid.get(i+1).get(j+1) == 'M') {
+                            if ((grid.get(i-1).get(j+1) == 'M' && grid.get(i+1).get(j-1) == 'S') || (grid.get(i-1).get(j+1) == 'S' && grid.get(i+1).get(j-1) == 'M')) {
+                               count2 += 1;
+                            }
+                        }
+                    }
+                }
             }
         }
-        System.out.println("Count: " + count);
+        System.out.println("xmas Count: " + count);
+        System.out.println("x-mas Count: " + count2);
     }
 }
